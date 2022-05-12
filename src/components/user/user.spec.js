77799a35a4ad9beb0c3cca318fa402eb1f55f77d
Nick Qi <nickqi@tubi.tv>
2022-05-12 23:26:21 +0800

    test: test the happy path when user API responds 200

---
+++ src/components/user/user.spec.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+
+import User from './User.component';
+import { getUser200 } from 'mocks/handlers';
+import { render, screen, waitFor } from 'utils/test';
+import { server } from 'mocks/server';
+
+describe('<User />', () => {
+  it('should fetch user data if fetch button is clicked', async () => {
+    server.use(getUser200);
+    render(<User />);
+    expect(screen.getByText('No user')).toBeInTheDocument();
+    expect(screen.queryByText('Fetching user...')).not.toBeInTheDocument();
+    userEvent.click(
+      screen.getByRole('button', {
+        name: 'Fetch user',
+      })
+    );
+    expect(screen.getByText('Fetching user...')).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.queryByText('No user')).not.toBeInTheDocument();
+    });
+  });
+});
