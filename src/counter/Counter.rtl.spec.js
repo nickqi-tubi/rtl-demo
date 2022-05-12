14cbc627f70879ec3155a54be4ebcfda5eebbed5
Nick Qi <nickqi@tubi.tv>
2022-05-12 17:56:06 +0800

    test: cover the same incremental test cases with RTL

---
+++ src/counter/Counter.rtl.spec.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import Counter from './Counter.component';
+
+describe('<Counter />', () => {
+  it('should set the passed initialCount to state', () => {
+    render(<Counter initialCount={2} />);
+    expect(screen.getByLabelText('count')).toHaveTextContent(2);
+  });
+
+  it('should increment by 1 when increment button is clicked', () => {
+    render(<Counter />);
+    expect(screen.getByLabelText('count')).toHaveTextContent(0);
+    userEvent.click(
+      screen.getByRole('button', {
+        name: 'Increment',
+      })
+    );
+    expect(screen.getByLabelText('count')).toHaveTextContent(1);
+  });
+});
